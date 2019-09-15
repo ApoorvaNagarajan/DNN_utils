9213b838779eb3d889f9427c5b88b8d3a69e0194
ApoorvaNagarajan <50163262+ApoorvaNagarajan@users.noreply.github.com>
2019-09-16 01:04:16 +0530

    Add files via upload

--- tfrecord_utils.py
+++ tfrecord_utils.py
@@ -66,7 +66,7 @@ def convert_to_tfRecord(dataset, trainFile=None, validFile=None, testFile=None):
       writerValid.close()
       sys.stdout.flush()
       
-    if((testFile != None) && (False == os.path.exists(testFile)):
+    if((testFile != None) and (False == os.path.exists(testFile)):
       for i in range(len(x_test)):
           # Create a feature
           feature = {
