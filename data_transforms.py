5f6ecd2a6075c38516893c0af7234f280d6297c3
ApoorvaNagarajan <50163262+ApoorvaNagarajan@users.noreply.github.com>
2019-09-20 00:06:59 +0530

    Update data_transforms.py

--- data_transforms.py
+++ data_transforms.py
@@ -244,5 +244,5 @@ def random_pad_crop(x: tf.Tensor, pad_size: int) -> tf.Tensor:
     
     shape = tf.shape(x)
     x = tf.pad(x, [[pad_size, pad_size], [pad_size, pad_size], [0, 0]])
-    x = tf.random_crop(x, [shape[0], shape[1], 3])
+    x = tf.image.random_crop(x, [shape[0], shape[1], 3])
     return x
